3129e86ad7bfdf15f1ca30285e571fe424f34a27
Nguyen Van Toan <toan.nguyenvan@atg.auto>
2024-01-12 15:25:50 +0700

    Game odd -even

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   name = 'Angular-app';
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  onIntervalFired(fireNumber: number) {
+    console.log(fireNumber);
+    if (fireNumber % 2 === 0) {
+      this.evenNumbers.push(fireNumber);
+    } else {
+      this.oddNumbers.push(fireNumber);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {ServerComponent} from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import {WarningAlertComponent} from './warning-alert/warning-alert.component';
 import {FormsModule} from '@angular/forms';
+import { EvenComponent } from './even/even.component';
+import { OddComponent } from './odd/odd.component';
+import { GameControlComponent } from './game-control/game-control.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    SuccessAlertComponent,
-    WarningAlertComponent,
+    EvenComponent,
+    OddComponent,
+    GameControlComponent,
   ],
   imports: [
     BrowserModule,
