3a98c4b02bd618641a805e85de9822524b93b9a6
Nguyen Van Toan <toan.nguyenvan@atg.auto>
2024-02-23 14:51:45 +0700

    Remove all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {ServerComponent} from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import {WarningAlertComponent} from './warning-alert/warning-alert.component';
 import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    SuccessAlertComponent,
-    WarningAlertComponent,
   ],
   imports: [
     BrowserModule,
